c1bc9d71367316ea70f5852d4f388e895750e24a
aaradhywrites <aaradhysrivastava1010@gmail.com>
2026-07-06 14:45:12 +0530

    lists

---
+++ user input and list.py
@@ -0,0 +1,10 @@
+movie = [   ]
+
+stg = (input("Enter name of your most favorite movies "))
+movie.append(stg)
+stg2 = (input("Enter name of your second most favorite movies "))
+movie.append(stg2)
+stg3 = (input("Enter name of your third most favorite movies "))
+movie.append(stg3)
+
+print ("Your favorite movies are: ", movie)
---
+++ user input and list3.py
@@ -0,0 +1,6 @@
+actor = []
+
+actor.append(input("Enter name of your most favorite actor: "))
+actor.append(input("Enter name of your second most favorite actor: "))  
+
+print("Your favorite actors are: ", actor)
