23fe1699dbbd65630032a9d9c95fd3d22119f82d
aaradhywrites <aaradhysrivastava1010@gmail.com>
2026-07-06 13:09:14 +0530

    Update greatest among 3 numbers.py

---
+++ greatest among 3 numbers.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number"))
+num2 = float(input("Enter 2nd number"))
+num3= float(input("Enter 3rd number"))
+
+if num1>num2 and num1>num3:
+    print ("First number is greatest")
+elif num1>num2 and num1<num3:
+    print ("Third number is the greatest")
