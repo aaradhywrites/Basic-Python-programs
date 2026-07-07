97edc4e53c7f9615a09be59607ff289b5f2cdbff
aaradhywrites <aaradhysrivastava1010@gmail.com>
2026-07-07 14:42:52 +0530

    new work

---
+++ script.py
@@ -0,0 +1,4 @@
+frnd = [ "aaradhy" , "rajvansh" , "vasnh" , 'saruabh']
+mainmsg = "So i guess all of your lifes is going good im learning python and i want to say you that i am very happy to have you as my friend"
+for friend in frnd:
+    print(" hey how are you, " + friend + "?" + mainmsg)
