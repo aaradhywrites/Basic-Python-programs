77e6640324a3beaa810200c33b57cf3f5f3ce13d
aaradhywrites <aaradhysrivastava1010@gmail.com>
2026-07-08 00:03:00 +0530

    new work cause m boreddddddddddd

---
+++ def fxn.py
@@ -0,0 +1,6 @@
+def g(name, department, role):
+    print("Name:", name)
+    print("Department:", department)
+    print("Role:", role)
+
+g("Aaradhy", "CyberSecurity", "Senior Executive Head")
---
+++ traiangle area.py
@@ -0,0 +1,7 @@
+def area_triangle(base, height):
+    return base*height/2
+
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+print("Area of triangle A:", area_a)
+print("Area of triangle B:", area_b)
